1582c16346e9eb7dad96679b780708664bbd9cb8
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-23 12:50:28 +0800

    config.go: replace AXION_BUILD_VARIANT with WITH_GMS
    Change-Id: Ic84ead98b1d692e358629664cb626eec6efd76e4

--- ui/build/config.go
+++ ui/build/config.go
@@ -1179,9 +1179,11 @@ func (c *configImpl) BuildFromTextStub() bool {
 }
 
 func (c *configImpl) TargetBuildGapps() bool {
-	if v, ok := c.environ.Get("AXION_BUILD_VARIANT"); ok {
+	if v, ok := c.environ.Get("WITH_GMS"); ok {
 		v = strings.TrimSpace(v)
-		return v == "GMS"
+		if v == "true" {
+			return true
+		}
 	}
 	return false
 }
